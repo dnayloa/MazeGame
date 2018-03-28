413275b0d4a9a7a1c8cf82030e19f1c868ee645b
dnayloa <dnayloa@essex.ac.uk>
2018-03-28 16:43:48 +0100

    Initial Main setup (test commit) dnayloa (28/03/18)

---
+++ .idea/src/com/dnayloa/main/Main.java
@@ -0,0 +1,19 @@
+package com.dnayloa.main;
+
+/**
+ * Created by dnayloa 28/03/2018
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void setup(){
+
+    }
+
+    public static void run(){
+
+    }
+}
